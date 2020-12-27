b420182943869dbab7fc7384ca7ecb2189135c7b
Andreas Steffensen <andreas.steffensen89@gmail.com>
2020-12-27 15:43:44 +0100

    updated gitignore file and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 const gulp = require('gulp');
 const { parallel, src, series, dest, watch } = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
+
+const jsPath = ('src/**/*.js');
+const buildPath = ('./build');
 
 function copyHtml(cb) {
-  return src('src/**/*.html').pipe(gulp.dest('public'));
+  return src('src/**/*.html').pipe(gulp.dest(buildPath));
+}
+
+function jsTask() {
+    return src(jsPath)
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(buildPath));
 }
 
 function css(cb) {
   // body omitted
-  return src('src/**/*.css').pipe(gulp.dest('public'));
+  return src('src/**/*.css').pipe(gulp.dest(buildPath));
 }
 
-exports.default = parallel(copyHtml, css);
+exports.jsTask = jsTask;
+
+exports.default = parallel(copyHtml, css, jsTask);
